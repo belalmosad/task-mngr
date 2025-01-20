1ef5d8f55a4e62eaf059451f35a5b743c2cd3f67
Belal ELmeligy <belalelmeligy516@gmail.com>
2025-01-20 10:03:32 +0200

    Create schema for task to validate on requests and responses

---
+++ back-end/src/schemas/task.py
@@ -0,0 +1,17 @@
+from marshmallow import Schema, fields
+
+class GetTaskSchema(Schema):
+    id = fields.Str(required=True)
+    name = fields.Str(required=True)
+    status = fields.String(required=True)
+    created_at = fields.DateTime(required=True)
+
+class CreateTaskSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+    status = fields.String(required=True)
+    
+    
+class UpdateTaskSchema(Schema):
+    name = fields.Str()
+    status=fields.Str()
