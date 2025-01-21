f51d282d3d76fa6774ea927db9051ad6f654b5fa
Belal ELmeligy <belalelmeligy516@gmail.com>
2025-01-21 17:54:47 +0200

    Create http service to handle http calls to backend

---
+++ front-end/src/app/http.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Task } from '../types/task.type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  baseRoute = '/service'
+  constructor(private http: HttpClient) { }
+
+  getTasks(): Observable<Object> {
+    return this.http.get(`${this.baseRoute}/tasks`);
+  }
+  addTask(data: any): Observable<object> {
+    return this.http.post(`${this.baseRoute}/tasks`, data);
+  }
+  deleteTask(taskId: any) {
+    return this.http.delete(`${this.baseRoute}/task/${taskId}`);
+  }
+}
