5ca696d926bb2a80d8a7071a0d9c5d335fea4f41
Belal ELmeligy <belalelmeligy516@gmail.com>
2025-01-22 10:22:58 +0200

    add update-task route

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { CreateTaskComponent } from './create-task/create-task.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
 
 export const routes: Routes = [
     {path: '', component: TaskListComponent},
-    {path: 'create-task', component: CreateTaskComponent}
+    {path: 'create-task', component: CreateTaskComponent},
+    {path: 'update-task', component: EditTaskComponent}
 ];
